49d9ffbc5c40ab090d3a3188a50dc0408430a28f
Jim Glansk <jim.glansk@mail.com>
2017-04-27 12:31:37 +0200

    Camera poop

--- Source/EngineName/Components/CInput.cs
+++ Source/EngineName/Components/CInput.cs
@@ -8,17 +8,21 @@ using Microsoft.Xna.Framework.Input;
 
 namespace EngineName.Components
 {
-    public class CInput:EcsComponent
-    {
-        public Keys ForwardMovementKey = Keys.W;
-        public Keys BackwardMovementKey = Keys.S;
-        public Keys LeftMovementKey = Keys.A;
-        public Keys RightMovementKey = Keys.D;
-        public Keys CameraMovementForward = Keys.Up;
-        public Keys CameraMovementBackward = Keys.Down;
-        public Keys CameraMovementRight = Keys.Right;
-        public Keys CameraMovementLeft = Keys.Left;
-        public Keys ZRotationPlus = Keys.Z;
-        public Keys ZRotationMinus = Keys.C;
-    }
+	public class CInput : EcsComponent
+	{
+		//public Keys ForwardMovementKey = Keys.W;
+		//public Keys BackwardMovementKey = Keys.S;
+		//public Keys LeftMovementKey = Keys.A;
+		//public Keys RightMovementKey = Keys.D;
+		public Keys CameraMovementForward = Keys.Up;
+		public Keys CameraMovementBackward = Keys.Down;
+		public Keys CameraMovementRight = Keys.Right;
+		public Keys CameraMovementLeft = Keys.Left;
+		public Keys CameraMovementUp = Keys.Space;
+		public Keys CameraMovementDown = Keys.LeftShift;
+		public Keys CameraTiltUp = Keys.W;
+		public Keys CameraTiltDown = Keys.S;
+		//public Keys ZRotationPlus = Keys.Z;
+		//
+	}
 }
--- Source/EngineName/Systems/InputSystem.cs
+++ Source/EngineName/Systems/InputSystem.cs
@@ -38,58 +38,33 @@ namespace EngineName.Systems {
                     }
                     if (currentState.IsKeyDown(inputValue.CameraMovementLeft)){
                         cameraComponent.Heading -= 0.05f;
-                        transform.Position = Vector3.Subtract(cameraComponent.Target, new Vector3((float)(cameraComponent.Distance * Math.Sin(cameraComponent.Heading + Math.PI * 0.5f)), cameraComponent.Height, (float)((-cameraComponent.Distance) * Math.Cos(cameraComponent.Heading + Math.PI * 0.5f))));
+                        transform.Position += Vector3.Subtract(cameraComponent.Target, new Vector3((float)(cameraComponent.Distance * Math.Sin(cameraComponent.Heading + Math.PI * 0.5f)), cameraComponent.Height, (float)((-cameraComponent.Distance) * Math.Cos(cameraComponent.Heading + Math.PI * 0.5f))));
                     }       
                     if (currentState.IsKeyDown(inputValue.CameraMovementRight)){
                         cameraComponent.Heading += 0.05f;
-                        transform.Position = Vector3.Subtract(cameraComponent.Target, new Vector3((float)(cameraComponent.Distance * Math.Sin(cameraComponent.Heading + Math.PI * 0.5f)), cameraComponent.Height, (float)((-cameraComponent.Distance) * Math.Cos(cameraComponent.Heading + Math.PI * 0.5f))));
-
+                        transform.Position += Vector3.Subtract(cameraComponent.Target, new Vector3((float)(cameraComponent.Distance * Math.Sin(cameraComponent.Heading + Math.PI * 0.5f)), cameraComponent.Height, (float)((-cameraComponent.Distance) * Math.Cos(cameraComponent.Heading + Math.PI * 0.5f))));
                     }
-                }
-
-                CBody body = null;
-                if(Game1.Inst.Scene.EntityHasComponent<CBody>(input.Key)){
-                    body = (CBody)Game1.Inst.Scene.GetComponentFromEntity<CBody>(input.Key);
-                }else{
-                    continue;
-                }
-                inputValue = (CInput)input.Value;
-
-                if (currentState.IsKeyDown(inputValue.ForwardMovementKey))
-                    body.Velocity.Z -= 5f;
-                if (currentState.IsKeyDown(inputValue.BackwardMovementKey))
-                    body.Velocity.Z += 5f;
-                if (currentState.IsKeyDown(inputValue.LeftMovementKey))
-                    body.Velocity.X -= 5f;
-                if (currentState.IsKeyDown(inputValue.RightMovementKey))
-                    body.Velocity.X += 5f;
-                
-                /*
-
-                //((LookAtCamera)Camera).Target = new Vector3(m.M41, m.M42*0.0f, m.M43);
-                //var ta = ((LookAtCamera)Camera).Target;
-                var p = b.Position;
-                var c = ((LookAtCamera)Camera).Position;
-                var dist = 30f;
-                var yDist = -20f;
-                var h = b.Heading;
-
-                // Vi positionerar kamera utifrån karaktärens heading (h), p = karaktärerns position, c = kamerans position, t = kamerans target, dist = avstånd till objektet
-                // yDist = höjd för kameran, samt t = p -- alltså att kamerans target är position för karaktären.
-                // Då gäller c=p-[d*sin(h + pi/2), y, (-d)*cos(h + pi/2)]
-
-                c = Vector3.Subtract(p, new Vector3((float)(dist * Math.Sin(h + Math.PI * 0.5f)), yDist, (float)((-dist) * Math.Cos(h + Math.PI * 0.5f))));
-
-                c.Y = -yDist; // Lock camera to given height
-                p.Y = 0; // Target too because it was really ugly otherwise
-
-                ((LookAtCamera)Camera).Target = p;
-                ((LookAtCamera)Camera).Position = c;
-                
-                return Matrix.CreateLookAt(Position, (Vector3)m_Target, Up);
-
-            */
-                
+					if (currentState.IsKeyDown(inputValue.CameraMovementUp))
+					{
+						transform.Position     += CAMERASPEED * 0.5f * new Vector3(0, 1, 0);
+						cameraComponent.Target += CAMERASPEED * 0.5f * new Vector3(0, 1, 0);
+                    }           
+					if (currentState.IsKeyDown(inputValue.CameraMovementDown))
+					{
+						transform.Position     += CAMERASPEED * 0.5f * new Vector3(0, -1, 0);
+						cameraComponent.Target += CAMERASPEED * 0.5f * new Vector3(0, -1, 0);
+					}
+                    if(currentState.IsKeyDown(inputValue.CameraTiltUp))
+					{
+						//transform.Position     += CAMERASPEED* 0.5f * new Vector3(0, -1, 0);
+						cameraComponent.Target += CAMERASPEED* 0.5f * new Vector3(0, 1, 0);
+					}
+					if(currentState.IsKeyDown(inputValue.CameraTiltDown))
+					{
+						//transform.Position     += CAMERASPEED* 0.5f * new Vector3(0, -1, 0);
+						cameraComponent.Target += CAMERASPEED* 0.5f * new Vector3(0, -1, 0);
+					}
+                }                
             }
         }
     }
